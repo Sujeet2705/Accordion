266583269ee5ae083e7cf6ea05054bcc60f57a2d
Sujith <sujeet.chinthakindi@gmail.com>
2024-10-24 12:59:44 +0530

    Added Multi Accordion

--- src/Components/accordianComponent.jsx
+++ src/Components/accordianComponent.jsx
@@ -5,27 +5,51 @@ import "../App.css";
 
 export default function AccordianComponent({ data }) {
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
   function handleSingleSelection(getCurrentId) {
     selected === getCurrentId ? setSelected(null) : setSelected(getCurrentId);
   }
 
+  function handleMultiSelection(getCurrentId) {
+    let copyMultiple = [...multiple];
+    copyMultiple.indexOf(getCurrentId) === -1
+      ? copyMultiple.push(getCurrentId)
+      : copyMultiple.splice(copyMultiple.indexOf(getCurrentId), 1);
+    console.log(copyMultiple);
+    setMultiple(copyMultiple);
+  }
+
+  function funToCall(dataItem) {
+    if (enableMultiSelection) {
+      handleMultiSelection(dataItem.id);
+    } else {
+      handleSingleSelection(dataItem.id);
+    }
+  }
+
   return (
     <div className="wrapper">
-      <button> Enable Multi Selection </button>
+      <button
+        onClick={() => {
+          setEnableMultiSelection(!enableMultiSelection);
+        }}
+      >
+        Enable Multi Selection
+      </button>
+
       <div className="accordion">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
-            <div className="item">
-              <div
-                onClick={() => handleSingleSelection(dataItem.id)}
-                className="title"
-              >
+            <div className="item" key={dataItem.id}>
+              <div onClick={() => funToCall(dataItem)} className="title">
                 <h3> {dataItem.question} </h3>
                 <span> + </span>
               </div>
               <div>
-                {dataItem.id === selected ? (
+                {dataItem.id === selected ||
+                multiple.indexOf(dataItem.id) !== -1 ? (
                   <div className="content"> {dataItem.answer} </div>
                 ) : null}
               </div>
